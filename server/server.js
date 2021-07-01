80784e022722924f141958c1b65df574655167c2
Elias Helander <eliashelander1@gmail.com>
2021-07-01 10:06:59 +0200

    Added mock data - ingredients

--- server/server.js
+++ server/server.js
@@ -1,12 +1,15 @@
 require("dotenv").config();
 const express = require('express');
 const cors = require('cors');
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+
+const adapter = new FileSync('./db/db.json')
+const db = low(adapter);
 
 const router = require('./apiRouter');
 
 const app = express();
-
-
 
 const PORT = process.env.PORT;
 
